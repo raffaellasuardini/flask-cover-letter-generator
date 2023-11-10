aae3f719b60627ba9dfa84e23606b3f6870d1896
raffaella <suardini.raffaella@gmail.com>
2023-11-10 19:21:06 +0100

    changed element color

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
 import Backdrop from "@mui/material/Backdrop";
 import Grid from "@mui/material/Grid";
 
@@ -14,11 +13,8 @@ function Loader() {
       alignItems="center"
       mt={5}
     >
-      <Backdrop
-        open="true"
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-      />
-      <CircularProgress color="inherit" />
+      <Backdrop open="true" sx={{ color: "#fff", zIndex: "0" }} />
+      <CircularProgress color="primary" />
       <Typography sx={{ color: "inherit" }} mt={3}>
         Stiamo costruendo la lettera
       </Typography>
