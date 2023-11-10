2a50c0e326a66562e9eca7ac7e7f34129909fcd6
raffaella <suardini.raffaella@gmail.com>
2023-11-10 19:20:25 +0100

    created button return in letter component

--- frontend/src/components/Letter.jsx
+++ frontend/src/components/Letter.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import RestartAltIcon from "@mui/icons-material/RestartAlt";
 
 function Letter(props) {
+  function handleRestart() {
+    window.location.reload();
+  }
   return (
     <React.Fragment>
       {props.content?.map((paragraph, index) => (
         <p index={index}>{paragraph}</p>
       ))}
+      <Button
+        variant="contained"
+        endIcon={<RestartAltIcon />}
+        onClick={handleRestart}
+      >
+        Restart
+      </Button>
     </React.Fragment>
   );
 }
